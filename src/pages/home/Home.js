6c6c5217bd51637268717b3f4c3664d693822b8a
Bam <khanguyen1204@gmail.com>
2023-12-03 00:37:21 +0700

    fix footer

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -5,7 +5,16 @@ import Navbar from "react-bootstrap/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <div>Home</div>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+      <p>1</p>
+    </>
+  );
 }
 
 export default Home;

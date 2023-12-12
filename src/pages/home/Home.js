527e036db4d640959d83bfe17578709fed871cf5
Bam <khanguyen1204@gmail.com>
2023-12-12 13:53:56 +0700

    feat: add introduction, mission to homepage

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -11,14 +11,49 @@ import NewsCarousel from "../../components/NewsCarousel/NewsCarousel";
 function Home() {
   return (
     <div>
+      <section class="aboutUs">
+        <div class="aboutUs_content">
+          <div class="about_left">
+            <img src={require("../../assets/intro2.jpg")} alt="" />
+          </div>
+          <div class="about_right">
+            <h1>Introduction</h1>
+            <p>
+              Books is your one-stop shop for all your bookish needs. We are an
+              online bookstore dedicated to providing our customers with a wide
+              selection of books at affordable prices. Whether you're looking
+              for the latest bestsellers, classic novels, or hidden gems, we
+              have something for everyone. We are passionate about books and
+              believe that everyone should have access to great reading
+              material.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section class="mission">
+        <div class="mission_content">
+          <div class="mission_left">
+            <h1>Mission</h1>
+            <p>
+              BaBooks is committed to fostering a love of reading by providing
+              everyone with accessible, diverse, and enriching literature. We
+              are committed to providing our customers with a positive and
+              friendly shopping experience.
+            </p>
+          </div>
+          <div class="mission_right">
+            <img src={require("../../assets/mission.jpg")} alt="" />
+          </div>
+        </div>
+      </section>
       <div className="otm">
         <h2>Also out this month</h2>
       </div>
       <CarouselBook />
-      <h2>News</h2>
+      <div class="news">
+        <h2>News</h2>
+      </div>
       <NewsCarousel />
-      <h2>Available in hardback</h2>
-      <h2>The latest news </h2>
     </div>
   );
 }

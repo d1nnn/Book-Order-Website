bb8a1a7cd995bebffcc183d26095f400424ea673
Bam <khanguyen1204@gmail.com>
2023-12-15 16:34:46 +0700

    feat: add data to home's news

--- src/components/NewsCarousel/NewsCarousel.js
+++ src/components/NewsCarousel/NewsCarousel.js
@@ -64,36 +64,39 @@ const NewsCarousel = () => {
   return (
     <div className="news_carousel">
       <Card className="card" style={{ width: "18rem" }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Img variant="top" src={require("../../assets/9bestbook.webp")} />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>
+            9 best books about listening to improve your communication skills
+          </Card.Title>
           <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            Listening is the underrated, overlooked stepchild to speaking, yet
+            it’s paramount for excellent communication.
           </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
+          <Button variant="primary">Read more</Button>
         </Card.Body>
       </Card>
       <Card className="card" style={{ width: "18rem" }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Img variant="top" src={require("../../assets/3benefit.webp")} />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>3 surprising benefits of rereading books</Card.Title>
           <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            Bibliophiles who reread books time and time again know the easy
+            comfort of sinking into a familiar story.
           </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
+          <Button variant="primary">Read more</Button>
         </Card.Body>
       </Card>
       <Card className="card" style={{ width: "18rem" }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Img variant="top" src={require("../../assets/14Ann.webp")} />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>14 best Ann Patchett books, ranked</Card.Title>
           <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            Trying to pick the best Ann Patchett book is like trying to choose
+            your favorite Taylor Swift song — there are way too many good ones,
+            which makes ranking almost impossible.
           </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
+          <Button variant="primary">Read more</Button>
         </Card.Body>
       </Card>
     </div>

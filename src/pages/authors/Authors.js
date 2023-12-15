4d4a4df6422a0293d60cfd39d2516504e75f0d9f
Bam <khanguyen1204@gmail.com>
2023-12-15 15:52:15 +0700

    feat: Add css to authors detail

--- src/pages/authors/Authors.js
+++ src/pages/authors/Authors.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useMemo, useState } from "react";
 import BookDataService from "../../services/BookDataService";
 import { Outlet, useParams } from "react-router-dom";
-
+import "./Authors.css";
 const Authors = (props) => {
   const params = useParams();
   //   console.log(params.id);
@@ -25,12 +25,16 @@ const Authors = (props) => {
   return (
     <div>
       {author && (
-        <div>
-          <img src={`${author.imageUrl}`}></img>
-          <p>{author.name}</p>
-          <p>{author.authorOf}</p>
-          <div dangerouslySetInnerHTML={{ __html: author.biography }} />
-          <p>{author.category}</p>
+        <div className="author">
+          <div className="author_left">
+            <img src={`${author.imageUrl}`}></img>
+          </div>
+          <div className="author_content">
+            <h1>{author.name}</h1>
+            <h2>{author.authorOf}</h2>
+            <p dangerouslySetInnerHTML={{ __html: author.biography }} />
+            {/* <p>{author.category}</p> */}
+          </div>
         </div>
       )}
     </div>

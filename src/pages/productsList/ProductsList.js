329d8f62c3e599d630691cfe0a69f3c5140623a6
Bam <khanguyen1204@gmail.com>
2023-12-21 13:53:34 +0700

    feat: add search bar function to product list

--- src/pages/productsList/ProductsList.js
+++ src/pages/productsList/ProductsList.js
@@ -1,55 +1,64 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useLayoutEffect } from "react";
 import "./Products.css";
 import { NavLink } from "react-router-dom";
 import BookDataService from "../../services/BookDataService";
 import Loading from "../../components/Loading/Loading";
 
+const getFilteredItems = (query, products) => {
+  if (!query) return products;
+
+  return products.filter((book) => book.name.toLowerCase().includes(query));
+};
+
 const ProductsList = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [input, setInput] = useState("");
+  const [query, setQuery] = useState("");
+  // const [filteredItems, setFilteredItems] = useState([]);
+
+  const filteredItems = getFilteredItems(query, products);
+
   useEffect(() => {
     retrieveProducts();
-  }, []);
+  });
+
+  // useEffect(() => {
+  //   setFilteredItems(getFilteredItems(query, products));
+  // }, [query]);
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setQuery(value);
+    // setFilteredItems(getFilteredItems(query, products));
+  };
 
   const retrieveProducts = () => {
-    setLoading(true);
+    // ;
     BookDataService.getAllProducts()
       .then((res) => {
         setProducts(res.data);
         setLoading(false);
-        // const data = res.data;
       })
-      // .then((data) => {
-      //   const results = data.filter((book) => {
-      //     return {};
-      //   });
-      // })
       .catch((e) => {
         console.log(e);
       });
   };
 
-  const handleChange = (value) => {
-    setInput(value);
-    retrieveProducts(value);
-  };
-
   return (
     <div>
-      {loading && <Loading />}
+      {/* {loading && <Loading />} */}
       <h1 className="chonmon">Pick your books!</h1>
       <div className="searchbar">
         <input
           type="search"
           placeholder="Tìm kiếm"
           id="searchbar"
-          // onChange={(e) => handleChange(e.target.value)}
+          onChange={(e) => handleChange(e)}
         />
       </div>
       <section className="sanpham">
         <div className="sanpham_left" id="spl">
-          {products.map((product, i) => {
+          {filteredItems.map((product, i) => {
             return (
               <div key={i} className="sanpham_item">
                 <NavLink to={`${product._id}`}>
@@ -66,3 +75,28 @@ const ProductsList = () => {
 };
 
 export default ProductsList;
+
+// : products.maps((product, i) => {
+//   return (
+//     <div key={i} className="sanpham_item">
+//       <NavLink to={`${product._id}`}>
+//         <img src={`${product.imageUrl}`} alt="" />
+//         <p>{product.name}</p>
+//       </NavLink>
+//     </div>
+//   );
+// })}
+
+{
+  /* {filteredItems? */
+}
+// : products.maps((product, i) => {
+//   return (
+//     <div key={i} className="sanpham_item">
+//       <NavLink to={`${product._id}`}>
+//         <img src={`${product.imageUrl}`} alt="" />
+//         <p>{product.name}</p>
+//       </NavLink>
+//     </div>
+//   );
+// })}

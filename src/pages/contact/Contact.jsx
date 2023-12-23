3b6e1340e3cb88ebf68afece7770703b624fc6cf
Bam <khanguyen1204@gmail.com>
2023-12-23 11:13:45 +0700

    fix: update css

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -45,7 +45,7 @@ const Contact = () => {
             <div className="col-12 col-md-8 px-4 mt-4 mt-md-0 ml-5">
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15672.924961103905!2d106.8030541!3d10.8700089!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317527587e9ad5bf%3A0xafa66f9c8be3c91!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBDw7RuZyBuZ2jhu4cgVGjDtG5nIHRpbiAtIMSQSFFHIFRQLkhDTQ!5e0!3m2!1svi!2s!4v1702141153978!5m2!1svi!2s"
-                width="750"
+                width="90%"
                 height="500"
                 allowFullScreen
                 loading="lazy"

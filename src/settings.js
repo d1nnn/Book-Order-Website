1d44db439e5b86acba3ec63a7a23271afecd9ad4
unknown <21522922@gm.uit.edu.vn>
2024-03-12 20:22:54 +0700

    test02

--- src/settings.js
+++ src/settings.js
@@ -12,7 +12,7 @@ var generatePassword = (
 // export const url = "https://babook17-6it6ur392-seisr.vercel.app";
 // export const url = "https://babook17-1gghhr8jq-seisr.vercel.app";
 // export const url = "https://babook17-o8hlknxrx-seisr.vercel.app";
-export const url = "https://babook17-dz6nm9kzt-seisr.vercel.app";
+export const url = "https://bookstoreserverapi.vercel.app";
 
 export const is_authorzied = async (token = null) => {
   if (!token) {
